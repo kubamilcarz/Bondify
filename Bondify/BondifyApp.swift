dfb10d4d8746a1ba9f0fcec035e5ca406dfa3fe1
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-05-21 12:14:38 +0200

    Initial Commit

---
+++ Bondify/BondifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BondifyApp.swift
+//  Bondify
+//
+//  Created by Kuba Milcarz on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BondifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
